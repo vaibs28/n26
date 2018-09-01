ceda871adced1cddba681428895270ba18f4f311
HackerRank <hackerrank@example.com>
2018-09-01 07:13:00 +0000

    Saved at Sat Sep 01 2018 12:44:58 GMT+0530 (India Standard Time)

--- src/main/java/com/n26/Application.java
+++ src/main/java/com/n26/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@R
+@Con
 public class Application {
 
     public static void main(String... args) {
